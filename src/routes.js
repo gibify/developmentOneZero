bb1134b58dc9fea762c1c758f057c18383621355
gibify <evandrogibicoski@outlook.com>
2020-09-28 15:33:01 -0300

    refactoring routes

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ const router = new Router();
 const userController = require('./controllers/userController');
 
 router.get('/', userController.index);
-router.get('/users', userController.index);
+router.get('/users', userController.list);
+router.get('/users/:id', userController.index);
   
-router.post('/user', userController.create);
+router.post('/users', userController.create);
+router.delete('/users/:id', userController.delete);
+router.put('/users/:id', userController.edit);
 
 module.exports = router;
